e373507fbba4f965c35204a6e8e9820f6d782187
laurahodge <57686486+laurahodge@users.noreply.github.com>
2019-12-03 17:04:03 -0500

    Add files via upload

---
+++ Chef and Server Objects.java
@@ -0,0 +1,7 @@
+Chef chef1=new Chef(String name, String employeeId, double hourlyWage);
+Chef chef2=new Chef(String name, String employeeId, double hourlyWage);
+Chef chef3=new Chef(String name, String employeeId, double hourlyWage);
+Server server1= new Server(String name, String employeeId, double hourlyWage);
+Server server2= new Server(String name, String employeeId, double hourlyWage);
+Server server3= new Server(String name, String employeeId, double hourlyWage);
+
