d7710ab7ae147c2a7779321c7d4b8b5b8946fb9a
laurahodge <57686486+laurahodge@users.noreply.github.com>
2019-12-03 16:59:48 -0500

    Update Chef.java

--- Chef.java
+++ Chef.java
@@ -1,14 +1,8 @@
 public class Chef extends Employee{
  // kitchen class to cook, cooking, and cooked (remove method)
  // hourly wage, double time stamp in/out, hours worked (out-in)
-  protected double hourlyWage;
   public Chef(String name, String employeeId, double hourlyWage){
-    super(name, employeeId);
-    this.hourlyWage=hourlyWage;}
-  public double getHourlyWage(){
-    return this.hourlyWage;}
-  public void setHourlyWage(double hourlyWage){
-    this.hourlyWage=hourlyWage;}
+    super(name, employeeId, hourlyWage);}
   public double timeIn(){
     //just here for declaration (use computer time)
     return hourlyWage;}
