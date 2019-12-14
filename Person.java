588e00a535d28d57a2de483cad4147fc3758c7f4
laurahodge <57686486+laurahodge@users.noreply.github.com>
2019-12-14 13:45:35 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,10 +1,19 @@
+/**
+*
+* @author laurahodge
+* Class that creates a Person.
+*/
 public class Person{
   protected String name;
-  public Person(String name){
+  /**
+  * Constructor for Person Class
+  * @param name: Name of person
+  */
+  public Person(String name){ //
     this.name=name;}
-  public String getName(){
+  public String getName(){ //getter for name
     return this.name;}
-  public void setName(String name){
+  public void setName(String name){ //setter for name
     this.name=name;}
 }
   
