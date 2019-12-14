65876b6531b384941ab950e49f8dd140309195c8
laurahodge <57686486+laurahodge@users.noreply.github.com>
2019-12-14 12:17:20 -0500

    Update Chef.java

--- Chef.java
+++ Chef.java
@@ -1,3 +1,9 @@
+/**
+*
+* @author lahod
+* Class that allows a chef to clock in and out using the computer time to calculate their hours worked.
+*/
+
 public class Chef extends Employee{
  // kitchen class to cook, cooking, and cooked (remove method)
  // hourly wage, double time stamp in/out, hours worked (out-in)
