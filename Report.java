2481f8dea3da1c6bcd4d11c2b87be3eb424bcf5b
aconway106 <57686435+aconway106@users.noreply.github.com>
2019-12-14 14:42:18 -0500

    Update Report.java

--- Report.java
+++ Report.java
@@ -18,5 +18,21 @@ public class Report {
 			total += e.getAmountMade();
 		return total;
 	}
-	
+	public String mostPopularDish(ArrayList<Dish> cooked, ArrayList<Dish> dishes) {
+		ArrayList<Integer> numOrdered = new ArrayList<Integer>(dishes.size());
+		for(int i = 0; i < numOrdered.size(); i++) {
+			numordered[i] = 0;
+		}	
+		for(Dish d: cooked) {
+			public int x = dishes.indexOf(d);
+			numOrdered[x]++;
+		}
+		Integer maxValue = numOrdered[0];
+		for(int j = 1; j < numOrdered.size(); j++) {
+			if(numOrdered[i] > maxValue;)
+				maxValue = numOrdered[j];
+		}
+		indexOfMostOrdered = numOrdered.indexOf(maxValue);
+		return dishes[indexOfMostOrdered].getDishName();
+	}
 }
