145010981bfb73d40fa996166e31c1a26c4bea83
laurahodge <57686486+laurahodge@users.noreply.github.com>
2019-12-14 13:07:18 -0500

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -2,10 +2,11 @@ public class Employee extends Person{
   protected String employeeId;
   protected double hourlyWage;
   protected double amountMade;
-  public Employee(String name, String employeeId, double hourlyWage){
+  public Employee(String name, String employeeId, double hourlyWage, double amountMade){
     super(name);
     this.employeeId=employeeId;
-    this.hourlyWage=hourlyWage;}
+    this.hourlyWage=hourlyWage;
+    this.amountMade=0;}
   public String getEmployeeId(){
     return this.employeeId;}
   public void setEmployeeId(String employeeId){
