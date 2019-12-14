123438cc6c1ba858ddceecfe8684518bf81c9944
laurahodge <57686486+laurahodge@users.noreply.github.com>
2019-12-14 13:52:05 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,19 +1,28 @@
 import java.util.ArrayList;
+/**
+*
+* @author laurahodge
+* Class that creates the Server which is a child of Employee.
+*/
 public class Server extends Employee{
   protected double tips;
   ArrayList <Table> tables=new ArrayList<Table>();
+  /**
+  * Constructor for Server Class
+  * @param name: Name of server
+  * @param employeeId: Employee Id for server
+  * @param hourlyWage: Amount server makes per hour
+  * @param amountMade: Amount server made that day
+  */
   public Server(String name, String employeeId, double hourlyWage, double amountMade){
     super(name, employeeId, hourlyWage, amountMade);
     tips=0;}
-  public void addTips(double amount){
+  public void addTips(double amount){ //adds tips to amount
     tips+=amount;}
-  /*public void fillTables(ArrayList <Customer> tables){
-    int tableNum=this.getCustomer().getTableNumber();
-    if (tables.get(tableNum)==null){
-      tables.set(tableNum)=this.Person().getName();}
-    else{
-      System.out.println("Table is in use, please choose a different table.");}
-  }  */
+  public double getTips(){ //getter for tips
+    return this.tips;}
+  public void setTips(double tips){ //setter for tips
+    this.tips=tips;}
 }
     
   
