97211fd1257c85a2f578e047a1e36d2e99736f4b
laurahodge <57686486+laurahodge@users.noreply.github.com>
2019-12-14 13:43:48 -0500

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,22 +1,34 @@
+/**
+*
+* @author laurahodge
+* Class that allows the Employee to be a Person and have an employeeId, hourlyWage, and amount they made
+*/
 public class Employee extends Person{
   protected String employeeId;
   protected double hourlyWage;
   protected double amountMade;
+  /**
+  * Constructor for the Employee Class
+  * @param name: Name of Employee
+  * @param employeeId: Id for employee
+  * @param hourlyWage: amount the employee makes hourly
+  * @param amountMade: amount the employee made for the day
+  */
   public Employee(String name, String employeeId, double hourlyWage, double amountMade){
     super(name);
     this.employeeId=employeeId;
     this.hourlyWage=hourlyWage;
     this.amountMade=0;}
-  public String getEmployeeId(){
+  public String getEmployeeId(){ //getter for employeeId
     return this.employeeId;}
-  public void setEmployeeId(String employeeId){
+  public void setEmployeeId(String employeeId){ //setter for employeeId
     this.employeeId=employeeId;}
-  public double getHourlyWage(){
+  public double getHourlyWage(){ //getter for hourlyWage
     return this.hourlyWage;}
-  public void setHourlyWage(double hourlyWage){
+  public void setHourlyWage(double hourlyWage){ //setter for hourlyWage
     this.hourlyWage=hourlyWage;}
-  public void setAmountMade(double d){
+  public void setAmountMade(double d){ //setter for amount made
     this.amountMade = d;}
-  public double getAmountMade(){
+  public double getAmountMade(){ //getter for amount made
     return this.amountMade;}
 }
